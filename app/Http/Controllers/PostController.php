c12bd0b19999e5928156ded6cd95081f9722dc91
Jose Solis <j.solisgom@gmail.com>
2021-04-18 22:37:55 -0400

    Implementación de vista de categorias

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,6 @@ class PostController extends Controller
     }
 
     public function show(Post $post){
-
         $similares = Post::where('category_id', $post->category_id)
                         ->where('status', 2)
                         ->where('id', '!=', $post->id)
@@ -24,4 +24,12 @@ class PostController extends Controller
 
         return view('posts.show', compact('post', 'similares'));
     }
-}
+
+    public function category(Category $category){
+        $posts = Post::where('category_id', $category->id)
+                        ->where('status', 2)
+                        ->latest('id')
+                        ->paginate(6);
+        return view('posts.category', compact('posts', 'category'));
+    }
+ }
