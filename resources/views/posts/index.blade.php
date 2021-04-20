10bc9665883f14d9ba9939bdfbfabb6b02707870
Jose Solis <j.solisgom@gmail.com>
2021-04-19 22:01:33 -0400

    ajuste menor

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,9 @@
             @foreach ($posts as $post)
                 <article class="w-full h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif" style="background-image: url({{ Storage::url($post->image->url) }})">
                     <div class="w-full h-full px-8 flex flex-col justify-center">
-                        <div>
+                        <div class="pb-4">
                             @foreach ($post->tags as $tag)
-                                <a href="" class="inline-block px-3 h-6 bg-{{$tag->color}}-600 text-white rounded-full">{{$tag->name}}</a>
-                                
+                                <a href="{{route('posts.tag', $tag)}}" class="inline-block px-3 h-6 bg-{{$tag->color}}-600 text-white rounded-full">{{$tag->name}}</a>
                             @endforeach
                         </div>
                         <h1 class="text-4xl text-white leading-8 font-bold">
